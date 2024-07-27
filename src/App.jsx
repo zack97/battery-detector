8228ba3b61e856832a9b213f451e35f25f1f32e1
zack10 <zacksmetz@yahoo.com>
2024-07-28 01:23:28 +0200

    updating the style 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,33 @@
 import React, { useEffect, useState } from "react";
 
-const App = () => {
-  const [battery, setBattery] = useState({ level: 0, charging: false });
+const BatteryDetector = () => {
+  const [batteryLevel, setBatteryLevel] = useState(0);
+  const [charging, setCharging] = useState(false);
+  const [batteryStatus, setBatteryStatus] = useState("");
 
   useEffect(() => {
-    const updateBattery = (batt) => {
-      setBattery({ level: batt.level, charging: batt.charging });
-
-      const batteryLiquid = document.querySelector(".Bliquid");
-      const batteryStatus = document.querySelector(".Bstatus");
-      const Bpercentage = document.querySelector(".Bpercentage");
-
-      const level = Math.floor(batt.level * 100);
-      Bpercentage.innerHTML = `${level}%`;
-      batteryLiquid.style.height = `${level}%`;
-
-      if (level === 100) {
-        batteryStatus.innerHTML = `Battery Full <i class="ri-battery-2-fill green-color"></i>`;
-        batteryLiquid.style.height = "103%";
-      } else if (level <= 20 && !batt.charging) {
-        batteryStatus.innerHTML = `Low Charge <i class="ri-plug-line animated-red"></i>`;
-      } else if (batt.charging) {
-        batteryStatus.innerHTML = `Charging ... <i class="ri-flashlight-line animated-green"></i>`;
+    const updateBatteryStatus = (battery) => {
+      setBatteryLevel(Math.floor(battery.level * 100));
+      setCharging(battery.charging);
+      if (battery.level === 1) {
+        setBatteryStatus("Battery Full");
+      } else if (battery.level <= 0.2 && !battery.charging) {
+        setBatteryStatus("Low Charge");
+      } else if (battery.charging) {
+        setBatteryStatus("Charging ...");
       } else {
-        batteryStatus.innerHTML = "";
-      }
-
-      if (level <= 20) {
-        batteryLiquid.classList.add("gradient-color-red");
-        batteryLiquid.classList.remove(
-          "gradient-color-green",
-          "gradient-color-orange",
-          "gradient-color-yellow"
-        );
-      } else if (level <= 48) {
-        batteryLiquid.classList.add("gradient-color-orange");
-        batteryLiquid.classList.remove(
-          "gradient-color-green",
-          "gradient-color-red",
-          "gradient-color-yellow"
-        );
-      } else if (level <= 80) {
-        batteryLiquid.classList.add("gradient-color-yellow");
-        batteryLiquid.classList.remove(
-          "gradient-color-green",
-          "gradient-color-orange",
-          "gradient-color-red"
-        );
-      } else {
-        batteryLiquid.classList.add("gradient-color-green");
-        batteryLiquid.classList.remove(
-          "gradient-color-red",
-          "gradient-color-orange",
-          "gradient-color-yellow"
-        );
+        setBatteryStatus("");
       }
     };
 
-    navigator.getBattery().then((batt) => {
-      updateBattery(batt);
-      batt.addEventListener("chargingchange", () => updateBattery(batt));
-      batt.addEventListener("levelchange", () => updateBattery(batt));
+    navigator.getBattery().then((battery) => {
+      updateBatteryStatus(battery);
+      battery.addEventListener("chargingchange", () =>
+        updateBatteryStatus(battery)
+      );
+      battery.addEventListener("levelchange", () =>
+        updateBatteryStatus(battery)
+      );
     });
   }, []);
 
@@ -69,14 +36,18 @@ const App = () => {
       <div className="Bcard">
         <div className="Bpill">
           <div className="Blevel">
-            <div className="Bliquid"></div>
+            <div
+              className="Bliquid"
+              style={{ height: `${batteryLevel}%` }}
+            ></div>
           </div>
         </div>
         <div className="Bdata">
-          <p className="Btext">Battery :</p>
-          <h1 className="Bpercentage">20%</h1>
+          <p className="Btext">Battery:</p>
+          <h1 className="Bpercentage">{batteryLevel}%</h1>
           <p className="Bstatus">
-            <i className="ri-plug-line"></i>
+            {batteryStatus}
+            {charging && <i className="ri-flashlight-line animated-green"></i>}
           </p>
         </div>
       </div>
@@ -84,4 +55,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default BatteryDetector;
